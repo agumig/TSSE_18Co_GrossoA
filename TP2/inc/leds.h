3645ccb3740617b3b7fe1063ab079a717f4c8a12
agumig <grosso811@gmail.com>
2023-05-15 20:21:53 -0300

    Se agregan comentarios al código

--- TP2/inc/leds.h
+++ TP2/inc/leds.h
@@ -5,12 +5,46 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/*** function prototypes ***/
+
+/**
+ * @brief   Función de inicialización del array de leds
+ * @details Inicia los led como apagados
+ */
 void LedsInit(uint16_t *);
+
+/**
+ * @brief Encendido de un led
+ * 
+ * @param led número de led a encender
+ */
 void LedsTurnOnSingle(uint8_t led);
+
+/**
+ * @brief Apagado de un led
+ * 
+ * @param led número de led a apagara
+ */
 void LedsTurnOffSingle(uint8_t led);
+
+/**
+ * @brief Adquisición del estado de un led
+ * 
+ * @param led número de led a consultar
+ * @return true si está encendido
+ * @return false si está apagado
+ */
 bool IsLedTurn(uint8_t led);
+
+/**
+ * @brief Apagado de todos los leds
+ * 
+ */
 void LedsTurnOffAll(void);
+
+/**
+ * @brief Encendido de todos los leds
+ * 
+ */
 void LedsTurnOnAll(void);
 
 
--- TP2/src/leds.c
+++ TP2/src/leds.c
@@ -1,20 +1,26 @@
-
+/**** includes ****/
 #include "leds.h"
 
+/**** definitions ****/
 #define INDEX_OFFSET 1
 #define FIRST_BIT 1
-
 #define ALL_LEDS_OFF    0x0000
 #define SINGLE_LED_OFF  0
 
-static uint16_t *Direccion;
+/**** private function prototypes ****/
+static uint16_t IndexToMask(uint8_t led);
+
+/**** private variable ****/
+static uint16_t *Direccion; // se almacena el mapeo en memoria del array de leds
 
 
-uint16_t IndexToMask(uint8_t led)
+/**** private functions ****/
+static uint16_t IndexToMask(uint8_t led)
 {
     return (FIRST_BIT << (led-INDEX_OFFSET));
 }
 
+/**** public functions ****/
 void LedsInit(uint16_t * direccion)
 {
     Direccion = direccion;
